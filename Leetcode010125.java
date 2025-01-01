08d0083705d2e65629a27d1fc2ce3eb2b0da8f4c
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2025-01-01 16:28:04 +0530

    Create Leetcode010125.java

---
+++ Leetcode010125.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/maximum-score-after-splitting-a-string/description/?envType=daily-question&envId=2025-01-01
+public class Solution {
+    public int maxScore(String inputStr) {
+        int totalOnes = 0;
+        int zerosCount = 0;
+        int onesCount = 0;
+        int bestScore = Integer.MIN_VALUE;
+
+        // Count total ones in the string
+        for (char ch : inputStr.toCharArray()) {
+            if (ch == '1') totalOnes++;
+        }
+
+        // Traverse the string and calculate scores
+        for (int i = 0; i < inputStr.length() - 1; i++) {
+            if (inputStr.charAt(i) == '0') zerosCount++;
+            else onesCount++;
+
+            // Calculate score
+            int currentScore = zerosCount + (totalOnes - onesCount);
+            bestScore = Math.max(bestScore, currentScore);
+
+        }
+
+        return bestScore;
+    }
+}

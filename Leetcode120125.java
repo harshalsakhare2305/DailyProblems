fbedb00c0b67d7c6abc2b9c3392aebe1a9a25791
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2025-01-12 15:28:56 +0530

    Create Leetcode120125.java

---
+++ Leetcode120125.java
@@ -0,0 +1,47 @@
+class Solution {
+//https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/?envType=daily-question&envId=2025-01-12
+    public boolean canBeValid(String s, String locked) {
+        int length = s.length();
+
+        // If length of string is odd, return false.
+        if (length % 2 == 1) {
+            return false;
+        }
+
+        Stack<Integer> openBrackets = new Stack<>();
+        Stack<Integer> unlocked = new Stack<>();
+
+        // Iterate through the string to handle '(' and ')'
+        for (int i = 0; i < length; i++) {
+            if (locked.charAt(i) == '0') {
+                unlocked.push(i);
+            } else if (s.charAt(i) == '(') {
+                openBrackets.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!openBrackets.empty()) {
+                    openBrackets.pop();
+                } else if (!unlocked.empty()) {
+                    unlocked.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+      
+        while (
+            !openBrackets.empty() &&
+            !unlocked.empty() &&
+            openBrackets.peek() < unlocked.peek()
+        ) {
+            openBrackets.pop();
+            unlocked.pop();
+        }
+
+        if (!openBrackets.empty()) {
+            return false;
+        }
+
+        return true;
+    }
+}

caff65fbbe543a51713c602e8fe73422201fbb8a
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2025-02-15 14:22:36 +0530

    Create GFG150225.java

---
+++ GFG150225.java
@@ -0,0 +1,7 @@
+//https://www.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-bst/1
+class Solution {
+    Node LCA(Node root, Node n1, Node n2) {
+        // your code here.
+        return (root.data>n1.data && root.data>n2.data)?LCA(root.left,n1,n2):(root.data<n1.data && root.data<n2.data)?LCA(root.right,n1,n2):root;
+    }
+}
